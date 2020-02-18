d5e453e4b07780e8e2a3c9dca10c4d7510cdcb85
Trinity Terry <trinitycterry@gmail.com>
2020-02-18 11:26:01 -0600

    Update apiManager.js

--- src/scripts/articles/apiManager.js
+++ src/scripts/articles/apiManager.js
@@ -2,7 +2,7 @@ const apiManager = {
 	getUserNews() {
 		const activeUserId = sessionStorage.getItem("activeUsers");
 		return fetch(
-			`http://localhost:8088/news?userId=${activeUserId}`
+			`https://tct-nutshell.herokuapp.com/news?userId=${activeUserId}`
 		).then(resp => resp.json());
 	},
 	getSiteUrl() {
@@ -10,7 +10,7 @@ const apiManager = {
 		
 	},
 	addUserNews(newsObj) {
-		return fetch('http://localhost:8088/news', {
+		return fetch('https://tct-nutshell.herokuapp.com/news', {
 			// Replace "url" with your API's URL
 			method: 'POST',
 			headers: {
@@ -20,7 +20,7 @@ const apiManager = {
 		});
 	},
 	deleteUserNews(id) {
-		return fetch(`http://localhost:8088/news/${id}`, {
+		return fetch(`https://tct-nutshell.herokuapp.com/news/${id}`, {
 			// Replace "url" with your API's URL
 			method: 'DELETE'
 		});
@@ -28,12 +28,12 @@ const apiManager = {
 	getSingleNews(id) {
 		const activeUserId = sessionStorage.getItem("activeUsers");
 		return fetch(
-			`http://localhost:8088/news?userId=${activeUserId}&&id=${id}`
+			`https://tct-nutshell.herokuapp.com/news?userId=${activeUserId}&&id=${id}`
 		).then(resp => resp.json());
 	},
 	editUserNews(id, newsObj) {
 		return fetch(
-			`http://localhost:8088/news/${id}`,
+			`https://tct-nutshell.herokuapp.com/news/${id}`,
 			{
 				method: `PUT`,
 				headers: {
