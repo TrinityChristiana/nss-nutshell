e8b8da0cfa1f9be60971cd48fdc33ca4393bd049
Trinity Terry <trinitycterry@gmail.com>
2020-02-19 19:08:03 -0600

    Update apiManager.js

--- src/scripts/all-news/apiManager.js
+++ src/scripts/all-news/apiManager.js
@@ -1,7 +1,7 @@
 const apiManager = {
 	getFriends(){
 		const userid = sessionStorage.getItem("activeUsers");
-		return fetch(`http://localhost:8088/friends?friendUserId=${userid}`)
+		return fetch(`https://tct-nutshell.herokuapp.com/friends?friendUserId=${userid}`)
 		.then(resp => resp.json());
 	}
 };
