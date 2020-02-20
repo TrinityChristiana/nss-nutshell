9eedf369c1f949f61c34c988bdc517a0c028eafc
Trinity Terry <trinitycterry@gmail.com>
2020-02-20 17:22:50 -0600

    Update htmlmanager.js

--- src/scripts/auth/htmlmanager.js
+++ src/scripts/auth/htmlmanager.js
@@ -3,7 +3,7 @@ const htmlManager = {
     createModalSignUpHtml:(obj) => {
       return `<div class="Kmodal-card">
       <div class="Kmodal-card-text-container">
-        <div id="SignUp-form">
+        <form id="SignUp-form">
           <div class="x-icon"><i id="leave-modal" class="times icon"></i></div>
           <h1>Welcome to TwixBook!</h1>
           <h4>Please fill out this form to continue...</h4>
@@ -37,7 +37,7 @@ autocomplete="username"
         </fieldset>
 
           <button class ="ui secondary button" type="button" id="join-button">Join!</button>
-        </div>
+        </form>
       </div>
     </div>`
     }
